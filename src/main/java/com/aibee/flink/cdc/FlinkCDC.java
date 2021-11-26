b28221124048f15b8441107be1f59313fb49f88a
hbwang-bigdata <hbwang@aibee.com>
2021-11-26 16:57:55 +0800

    add mysql cdc 2 kafka

---
+++ src/main/java/com/aibee/flink/cdc/FlinkCDC.java
@@ -0,0 +1,89 @@
+package com.aibee.flink.cdc;
+//import com.ververica.cdc.connectors.mysql.MySqlSource;
+
+import com.aibee.flink.cdc.MyPartitioner;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+
+import java.util.Properties;
+
+
+public class FlinkCDC {
+
+    public static void main(String[] args) throws Exception {
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+
+        //开启Checkpoint,每隔5秒钟做一次CK
+        env.enableCheckpointing(5000L);
+        //指定CK的一致性语义
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        //设置任务关闭的时候保留最后一次CK数据
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //指定从CK自动重启策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
+        //设置状态后端
+        String checkpointDir ="file:///tmp/flink/checkpoints";
+        env.setStateBackend(new FsStateBackend(checkpointDir));
+
+        //my
+        String hostname = "172.16.244.60";
+        String user = "root";
+        String password = "123456";
+        String databases = "store_bi_system";
+        String tables = "store_bi_system.score,store_bi_system.common_domain";
+        //测试库
+//        String hostname = "data-db01.aibee.cn";
+//        String user = "openfaasdb_user";
+//        String password = "5apj5R7s$y75S8z3";
+//        String databases = "store_bi_system";
+//        String tables = "store_bi_system.common_daily_action_index_copy1";
+        //volvo
+//        String hostname = "rm-bp13896493j23216y.mysql.rds.aliyuncs.com";
+//        String user = "store_volvo_cc";
+//        String password = "lEMRxpjrym55BviJ";
+//        String databases = "store_bi_volvo";
+//        String tables = "store_bi_volvo.lx_test";
+
+
+        MySqlSource<String> mysqlSource = MySqlSource.<String>builder()
+                .hostname(hostname)
+                .port(3306)
+                .username(user)
+                .password(password)
+                .databaseList(databases)
+                //可选配置项,注意：指定的时候需要使用"db.table"的方式
+                .tableList(tables)
+                .startupOptions(StartupOptions.latest())
+//                .deserializer(new Flink_CDCWithCustomerSchema())
+//                .deserializer(new StringDebeziumDeserializationSchema())
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .build();
+
+
+        DataStreamSource<String> mysqlDS = env.fromSource(mysqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source");
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","172.24.6.129:9092,172.24.6.130:9092,172.24.6.131:9092");
+        mysqlDS.addSink(new FlinkKafkaProducer<String>("flink-metric-log",new SimpleStringSchema(), properties,java.util.Optional.of(new MyPartitioner()))).name("flinkinsertkafka");
+
+
+        mysqlDS.print();
+
+        //6.执行任务
+        env.execute();
+
+    }
+}
---
+++ src/main/java/com/aibee/flink/cdc/MyPartitioner.java
@@ -0,0 +1,24 @@
+package com.aibee.flink.cdc;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartitioner;
+
+public class MyPartitioner extends FlinkKafkaPartitioner {
+    @Override
+    public int partition(Object record, byte[] key, byte[] value, String targetTopic, int[] partitions) {
+        //这里接收到的key是上面MySchema()中序列化后的key，需要转成string，然后取key的hash值取模kafka分区数量
+        JSONObject jsonObject = JSONObject.parseObject(new String(value));
+        JSONObject source;
+        if(jsonObject.containsKey("payload")){
+            JSONObject payload = jsonObject.getJSONObject("payload");
+             source = payload.getJSONObject("source");
+        }else {
+             source = jsonObject.getJSONObject("source");
+        }
+        String db = source.getString("db");
+        String table = source.getString("table");
+        int part = Math.abs((db +  table).hashCode() % partitions.length);
+//        System.out.println("part:"+part+"+size:"+partitions.length);
+        return part;
+    }
+}
